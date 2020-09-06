966bb3fd1eee679d33b3149849eeef57279b4a6c
Akhilesh kapse <64481847+Adk2001tech@users.noreply.github.com>
2020-09-06 11:26:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-import os
+port os
 import numpy as np
 
-from flask import Flask, render_template, url_for, request, send_file  #############NEW send_file ################################
+from flask import Flask, render_template, url_for, request
+from flask import send_file                                      ###################################################NEW
 from werkzeug.utils import secure_filename
 
 
@@ -13,26 +14,47 @@ import matplotlib.pyplot as plt
 
 from io import BytesIO
 import base64
+import cv2
 
 import load_net
 
 
+# img= np.ones(shape=(20,20,3))
 
 
 app= Flask(__name__)
 
+img=None
+g_list= []
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
 
+
 @app.route('/Prediction', methods = ['GET', 'POST'])
 def pred():
+    global img, g_list
     if request.method=='POST':
          file = request.files['file']
          org_img, feed_img= load_net.process_file(file)
-         arr = load_net.out_via_model_sm(feed_img)
+         img = load_net.out_via_model_sm(feed_img)
+         g_list.append(img)
+         img_x=BytesIO()
+         plt.imshow(img)
+         plt.savefig(img_x,format='png')
+         plt.close()
+         img_x.seek(0)
+         plot_url1=base64.b64encode(img_x.getvalue()).decode('utf8')
+
+    return render_template('pred.html',  plot_url1=plot_url1)
+
+
+
+@app.route('/download', methods = ['GET', 'POST'])
+def download():
+         arr= g_list[0].copy()
          arr=arr*255.0
          img = Image.fromarray(arr.astype('uint8'))
          file_object= BytesIO()
@@ -40,24 +62,9 @@ def pred():
          file_object.seek(0)
          return send_file(file_object, mimetype='image/PNG',attachment_filename='pic.png',  as_attachment=True)
 
-#          #uploaded image
-#          img_x0=BytesIO()
-#          plt.imshow(org_img)
-#          plt.savefig(img_x0,format='png')
-#          plt.close()
-#          img_x0.seek(0)
-#          plot_url0=base64.b64encode(img_x0.getvalue()).decode('utf8')
 
-#         #Model 1
-#          img = load_net.out_via_model_sm(feed_img)
-#          img_x=BytesIO()
-#          plt.imshow(img)
-#          plt.savefig(img_x,format='png')
-#          plt.close()
-#          img_x.seek(0)
-#        plot_url1=base64.b64encode(img_x.getvalue()).decode('utf8')
-#     return render_template('pred.html', plot_url0=plot_url0,  plot_url1=plot_url1)
 
 
 if __name__=='__main__':
     app.run()
+
